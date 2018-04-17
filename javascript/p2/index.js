5668c112ff751e7d434be0d15322d83f814abe2b
risonakamo <hmykanae@gmail.com>
2018-04-17 15:17:37 -0400

    button functionality

--- javascript/p2/index.js
+++ javascript/p2/index.js
@@ -2,7 +2,38 @@ window.onload=main;
 
 function main()
 {
+    var namebar=document.querySelector("#name");
+    var msgbar=document.querySelector("#message");
 
+    var currentname=localStorage.getItem("currentname");
+    if (currentname)
+    {
+        namebar.value=currentname;
+    }
+
+    document.querySelector(".clear-bt").addEventListener("click",(e)=>{
+        e.preventDefault();
+
+        msgbar.value="";
+
+        msgbar.focus();
+    });
+
+    document.querySelector(".submit-bt").addEventListener("click",(e)=>{
+        e.preventDefault();
+
+        if (namebar.value=="" || msgbar.value=="")
+        {
+            console.log("hey");
+            namebar.focus();
+            return;
+        }
+
+        localStorage.setItem("currentname",namebar.value);
+        msgbar.value="";
+
+        msgbar.focus();
+    });
 }
 
 function testget()
@@ -18,4 +49,9 @@ function testget()
     };
 
     r.send();
+}
+
+function genMsg(msgdata)
+{
+    return `<div class="shout"><span class="timestamp">[${msgdata.time}]</span><span class="name">${msgdata.name}</span>:<span>${msgdata.message}</span></div>`;
 }
