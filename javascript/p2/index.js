a0513947bbadc07b94cd6cebfe15ed4c0c817a85
risonakamo <hmykanae@gmail.com>
2018-04-26 15:18:37 -0400

    trying things

--- javascript/p2/index.js
+++ javascript/p2/index.js
@@ -51,6 +51,24 @@ function testget()
     r.send();
 }
 
+function shoutSend(name,msg)
+{
+    var r=new XMLHttpRequest();
+    r.open("POST","server/shout.php");
+
+    r.onreadystatechange=()=>{
+        if (r.readyState==4)
+        {
+            console.log(r.response);
+        }
+    };
+
+    // console.log(`name=${encodeURI(name)}&message=${encodeURI(msg)}`);
+    r.setRequestHeader("Content-type","application/x-www-form-urlencoded")
+    // r.send("name=Samir&message=Oh%20no!%20Not%20again!%20Why%20does%20it%20say%20paper%20jam%20when%20there%20is%20no%20paper%20jam%3F!!");
+    r.send(`name=${encodeURI(name)}&message=${encodeURI(msg)}`);
+}
+
 function genMsg(msgdata)
 {
     return `<div class="shout"><span class="timestamp">[${msgdata.time}]</span><span class="name">${msgdata.name}</span>:<span>${msgdata.message}</span></div>`;
