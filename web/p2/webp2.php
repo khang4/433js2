bf02779e0af258484efc7a8e3cdb5ff63d75e25b
risonakamo <hmykanae@gmail.com>
2018-05-05 16:57:31 -0400

    size and type checks

--- web/p2/webp2.php
+++ web/p2/webp2.php
@@ -1,7 +1,7 @@
 <?php
     $filelistpath="../../../../cs433/read-write/p2/filelist.json";
 
-    if ($_SERVER['REQUEST_METHOD']=='GET')
+    if ($_SERVER["REQUEST_METHOD"]=="GET")
     {
         $filelist=json_decode(@file_get_contents($filelistpath,true));
 
@@ -13,7 +13,7 @@
         echo json_encode($filelist);
     }
 
-    else if ($_SERVER['REQUEST_METHOD']=='POST')
+    else if ($_SERVER["REQUEST_METHOD"]=="POST")
     {
         $filelist=json_decode(@file_get_contents($filelistpath,true));
 
@@ -25,23 +25,44 @@
         $finfo=finfo_open(FILEINFO_MIME_TYPE);
         foreach ($_FILES as $file)
         {
+            $realfiletype=finfo_file($finfo,$file["tmp_name"]);
+            $typeconvertfiletype=typeconvert($realfiletype);
+            if ($file["size"]>500000)
+            {
+                echo json_encode(array(
+                    "status"=>"invalidsize"
+                ));
+                return;
+            }
+
+            if ($typeconvertfiletype=="unsupported")
+            {
+                echo json_encode(array(
+                    "status"=>"invalidtype",
+                    "type"=>$realfiletype
+                ));
+                return;
+            }
+
             $filelist[]=array(
-                'name'=>$file['name'],
-                'size'=>humansize($file['size']),
-                'type'=>typeconvert(finfo_file($finfo,$file['tmp_name'])),
-                'modtime'=>filemtime($file['tmp_name'])
+                "name"=>$file["name"],
+                "size"=>humansize($file["size"]),
+                "type"=>typeconvert($realfiletype),
+                "modtime"=>filemtime($file["tmp_name"])
             );
         }
 
         file_put_contents($filelistpath,json_encode($filelist),LOCK_EX);
 
-        echo 'did it';
+        echo json_encode(array(
+            "status"=>"uploaded"
+        ));
     }
 
     //based on something i read online
     function humansize($size)
     {
-        $units=array('B','kB','MB','GB','TB','PB','EB','ZB','YB');
+        $units=array("B","kB","MB","GB","TB","PB","EB","ZB","YB");
         $i = 0;
 
         while (($size/1024)>.9)
@@ -57,22 +78,23 @@
     {
         switch ($type)
         {
-            case 'image/png':
-                return 'png';
+            case "image/png":
+                return "png";
 
-            case 'image/gif':
-                return 'gif';
+            case "image/gif":
+                return "gif";
 
-            case 'image/jpeg':
-                return 'jpeg';
+            case "image/jpeg":
+            case "image/jpg":
+                return "jpeg";
 
-            case 'text/plain':
-                return 'text';
+            case "text/plain":
+                return "text";
 
-            case 'text/html':
-                return 'html';
+            case "text/html":
+                return "html";
         }
 
-        return 'unsupported';
+        return "unsupported";
     }
 ?>
