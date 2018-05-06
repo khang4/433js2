32f6c46cbae308e8f1c8a953a5ceea386df96a58
risonakamo <hmykanae@gmail.com>
2018-05-05 23:53:45 -0400

    gen filelist

--- web/p2/webp2.js
+++ web/p2/webp2.js
@@ -1,8 +1,12 @@
 window.onload=main;
 
+var _filelist;
+
 function main()
 {
+    _filelist=document.querySelector(".file-list");
 
+    updatefilelist();
 }
 
 function testupload()
@@ -28,4 +32,43 @@ function testupload()
     }
 
     r.send(uploadform);
+}
+
+function getfilelist(callback)
+{
+    var r=new XMLHttpRequest();
+    r.open("GET","webp2.php");
+
+    r.onreadystatechange=()=>{
+        if (r.readyState==4)
+        {
+            callback(JSON.parse(r.response));
+        }
+    };
+
+    r.send();
+}
+
+function updatefilelist()
+{
+    getfilelist((data)=>{
+        console.log(data);
+
+        for (var x=0,l=data.length;x<l;x++)
+        {
+            _filelist.appendChild(genfilelistentry(data[x]));
+        }
+    });
+}
+
+//give it single data entry from filelist
+function genfilelistentry(data)
+{
+    var res=document.createElement("div");
+
+    data.modtime=new Date(data.modtime*1000);
+    data.modtime=`${data.modtime.toISOString().slice(0,10)} ${data.modtime.toTimeString().slice(0,8)}`;
+
+    res.innerHTML=`<dl><dt>${data.name}</dt><dd>${data.size}</dd><dd class="type">${data.type}</dd><dd class="mod-time">${data.modtime}</dd><dd class="delete"><span>delete</span></dd></dl>`;
+    return res.firstElementChild;
 }
